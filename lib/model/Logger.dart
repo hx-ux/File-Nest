d63bb940f9f97418ca9dde52c0c57b49abfc3ce6
Your Name <itsmecettle@gmail.com>
2024-11-30 14:53:59 +0100

    moved log enum to file

--- lib/model/Logger.dart
+++ lib/model/Logger.dart
@@ -1,57 +1,10 @@
 import 'dart:io';
 import 'package:file_nest/config.dart';
+import 'package:file_nest/model/log_level.dart';
 import 'package:file_nest/services/Dialogs.dart';
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
-enum LogLevel {
-  debug(
-    attentioncolor: Colors.blue,
-    icon: FluentIcons.bug_24_regular,
-    description: "Success",
-  ),
-  info(
-    attentioncolor: Colors.green,
-    icon: FluentIcons.info_24_regular,
-    description: "Success",
-  ),
-  warning(
-    attentioncolor: Colors.orange,
-    icon: FluentIcons.text_bullet_list_24_regular,
-    description: "Error",
-  ),
-  error(
-    attentioncolor: Colors.red,
-    icon: FluentIcons.text_bullet_list_24_regular,
-    description: "Error",
-  ),
-  copy(
-    attentioncolor: Colors.green,
-    icon: FluentIcons.copy_arrow_right_24_regular,
-    description: "Success",
-  ),
-  move(
-    attentioncolor: Colors.green,
-    icon: FluentIcons.arrow_move_24_regular,
-    description: "Success",
-  ),
-  duplicate(
-    attentioncolor: Colors.grey,
-    icon: FluentIcons.bug_24_regular,
-    description: "Unknown",
-  );
-
-  final Color attentioncolor;
-  final IconData icon;
-  final String description;
-
-  const LogLevel({
-    required this.attentioncolor,
-    required this.icon,
-    required this.description,
-  });
-}
 
 class AppLogger {
   LogLevel logLevel;
@@ -64,8 +17,7 @@ class AppLogger {
 
   // DateFormat DateTimeFormat() => DateFormat('dd/MM/yy HH:mm:ss');
   String GetTimeStampAsString() {
-    var formatter = DateFormat(
-        'dd.MM.yyyy HH:mm:ss', 'de_DE'); 
+    var formatter = DateFormat('dd.MM.yyyy HH:mm:ss', 'de_DE');
     return formatter.format(DateTime.now());
   }
 
@@ -89,7 +41,7 @@ class AppLogger {
     if (showSnackbar) {
       showSnackbarInformation(
           message, fileName != "none" ? fileName : destination,
-          infoType: logLevel.description);
+          infoType: logLevel);
     }
   }
 
@@ -112,20 +64,24 @@ class AppLogger {
 
   AppLogger _logToClass(String message) {
     {
-      var parts = message.split('|');
-      AppLogger appLogger = AppLogger();
-      appLogger.logLevel = LogLevel.values.byName(parts[0]);
-      appLogger.timestamp = parts[1];
-      appLogger.message = parts[2];
-      appLogger.fileName = parts[3];
-      appLogger.destination = parts[4];
-      appLogger.icon = LogLevel.values.byName(parts[0]).icon;
-      appLogger.color = LogLevel.values.byName(parts[0]).attentioncolor;
-      return appLogger;
+      try {
+        var parts = message.split('|');
+        AppLogger appLogger = AppLogger();
+        appLogger.logLevel = LogLevel.values.byName(parts[0]);
+        appLogger.timestamp = parts[1];
+        appLogger.message = parts[2];
+        appLogger.fileName = parts[3];
+        appLogger.destination = parts[4];
+        appLogger.icon = LogLevel.values.byName(parts[0]).icon;
+        appLogger.color = LogLevel.values.byName(parts[0]).attentioncolor;
+        return appLogger;
+      } catch (e) {
+        return new AppLogger();
+      }
     }
   }
 
   void showSnackbarInformation(String msg, String? fileName,
           {required infoType}) =>
-      DialogSimpleConfirm(LogLevel.info, msg, fileName, showDuration: 1000);
+      DialogSimpleConfirm(infoType, msg, fileName, showDuration: 1000);
 }
